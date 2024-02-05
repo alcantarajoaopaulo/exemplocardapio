df9fe61656b76820fef360a9bfe633f684df83b0
João Paulo Alcântara <alcantarajp.silva@gmail.com>
2024-02-04 23:29:44 -0300

    Continuação do componente ProdutoVitrine
    Correção de atributo CSS "cursor" na classe .btn do CSS Global

--- src/components/produto-vitrine/produto-vitrine.jsx
+++ src/components/produto-vitrine/produto-vitrine.jsx
@@ -1,8 +1,21 @@
 import "./produto-vitrine.css";
+import foto from "../../assets/hamburguer.png";
+import bag from "../../assets/bolsa.png";
 
 function ProdutoVitrine(){
     return <div className="produto-box text-center">
-        Produto
+        <img src={foto} alt="Imagem"/>
+        <div>
+            <h2>Spice Burguer</h2>
+            <p className="prod-vitrine-descricao">Hamburguer de 250g, queijo, tomate, alface e cebola</p>
+            <p className="prod-vitrine-preco">R$ 18,90</p>
+        </div>
+        <div>
+            <button className="btn btn-cart">
+                <img src={bag} className="icon" alt="Bag-Icon"/>
+                Adicionar
+            </button>
+        </div>
     </div>
 
 }
